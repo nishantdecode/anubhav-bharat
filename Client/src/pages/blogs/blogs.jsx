da74f9d30762f62f373999ea836c40a19e7a7f5b
TSET04 <tejasethi22@gmail.com>
2023-03-17 20:10:16 +0530

    blogs

--- Client/src/pages/blogs/blogs.jsx
+++ Client/src/pages/blogs/blogs.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Grid, Box } from '@mui/material';
 import Navbar from "../../components/navbar/navbar.jsx";
 import Footer from '../../components/footer/footer.jsx';
+import BlogSection from '../../components/blogSection/blogSection.jsx';
 
 const Blogs = () => {
   return (
     <Box>
         <Navbar active="Blogs" />
+        <Grid container spacing={10} justifyContent="center" alignItems="center" my={-4}>
+        <Grid item xs={12} md={10}>
+            <BlogSection/>
+          </Grid>
+        </Grid>
         <Footer/>
     </Box>
   )
