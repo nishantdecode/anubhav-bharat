175e4dfcca2bfa1802092dab290fb492f375095b
TSET04 <tejasethi22@gmail.com>
2023-03-18 22:00:50 +0530

    Develop main daala

--- Client/src/components/head/head.jsx
+++ Client/src/components/head/head.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import {Box, styled} from "@mui/material";
-import Bg from "../../images/searchBg.jpg"
+import {Box} from "@mui/material";
+import Bg from "../../images/Spiritual.jpg"
 
-const Div = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-  padding: theme.spacing(1),
-}));
-
-const SearchBar = () => {
+const Head = () => {
   
   return (
-        <Box sx={{display:'flex', width: '100%', height:'400px', justifyContent:'center', alignItems:'center', backgroundImage:`url(${Bg})`, backgroundRepeat: "no-repeat", backgroundPosition: "bottom right", backgroundSize: "cover", backgroundAttachment: "fixed", borderRadius:'16px'}}>
-        <Div>Discover the diversity and richness of India: A world within a country.</Div>
+        <Box sx={{display:'flex', width: '100%', height:'400px',justifyContent:'center', backgroundcolor:'Black', opacity:'0.8', alignItems:'center', backgroundImage:`url(${Bg})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", backgroundAttachment: "fixed", borderRadius:'16px'}}>
+        
+        <Box variant='h2' align='center' color={'white'} fontSize = '3.5rem' fontWeight={500}>Discover the diversity and richness of India: A world within a country.</Box>
         </Box>
   )
 }
 
-export default SearchBar
+export default Head
--- Client/src/pages/blogs/blogs.jsx
+++ Client/src/pages/blogs/blogs.jsx
@@ -5,20 +5,22 @@ import Footer from '../../components/footer/footer.jsx';
 import BlogSection from '../../components/blogSection/blogSection.jsx';
 import TourCardRow from '../../components/tourCardRow/tourCardRow.jsx';
 import Bestseller from '../../data/bestseller.js';
-import Testimonials from '../../components/testimonials/testimonials.jsx';
+// import Testimonials from '../../components/testimonials/testimonials.jsx';
 import Head from '../../components/head/head.jsx';
+import DisplayCardRow from '../../components/displayCardRow/displayCardRow.jsx';
+import TouristAttractions from '../../data/touristAttraction.js';
 
 const Blogs = () => {
   return (
     <Box>
       <Navbar active="Blogs" />
       <Grid container spacing={10} justifyContent="center" alignItems="center" my={-4}>
-      <Grid item xs={12} md={10}>
-            <Head/>
-          </Grid>
-      <Grid item xs={12} md={10}>
-            <Testimonials/>
-          </Grid>
+        <Grid item xs={12} md={10}>
+          <Head />
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <DisplayCardRow data={TouristAttractions} titleHeading="PLACES TO VISIT" titleDesc="The amazing places around India!" />
+        </Grid>
         <Grid item xs={12} md={10}>
           <BlogSection />
         </Grid>
