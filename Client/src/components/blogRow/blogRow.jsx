2acbfe359cec357218f66229d0236b2a40056b78
TSET04 <tejasethi22@gmail.com>
2023-03-20 13:46:07 +0530

    blog head modified

--- Client/src/components/blogRow/blogRow.jsx
+++ Client/src/components/blogRow/blogRow.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box} from "@mui/material";
+import {Typography, Box} from "@mui/material";
 import Lg from "../../images/lodhigarden.jpg";
 import hc from "../../images/hc4.jpg";
 import food from "../../images/food5.jpeg";
@@ -10,21 +10,21 @@ import Bc from "../../images/butterchicken.webp";
 const BlogRow = () => {
   
   return (
-        <Box sx={{display:'flex', justifyContent:'space-evenly'}}>
+        <Box sx={{display:'flex', gap:'15px', overflow:'scroll'}}>
         <Box sx={{display:'flex', width: '200px', height:'270px', justifyContent:'center', backgroundcolor:'Black', backgroundBlendMode:'Darken', background: 'rgba(0,0,0,0.5)', opacity:'0.8', alignItems:'center', backgroundImage:`url(${Lg})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", borderRadius:'16px', }}>
-        <Box sx={{textAlign:'center', color:'white', fontSize:'2rem', fontWeight:'500'}}><h6>Heritage Blogs</h6></Box>
+        <Typography Align='center' color='white' variant='h6' fontWeight='500'>Heritage Blogs</Typography>
         </Box>
         <Box sx={{display:'flex', width: '200px', height:'270px', justifyContent:'center', backgroundcolor:'Black', backgroundBlendMode:'Darken', background: 'rgba(0,0,0,0.5)', opacity:'0.8', alignItems:'center', backgroundImage:`url(${hc})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", borderRadius:'16px', }}>
-        <Box sx={{textAlign:'center', color:'white', fontSize:'2rem', fontWeight:'500'}}><h6>Cultural Blogs</h6></Box>
+        <Typography Align='center' color='white' variant='h6' fontWeight='500'>Cultural Blogs</Typography>
         </Box>
         <Box sx={{display:'flex', width: '200px', height:'270px', justifyContent:'center', backgroundcolor:'Black', backgroundBlendMode:'Darken', background: 'rgba(0,0,0,0.5)', opacity:'0.8', alignItems:'center', backgroundImage:`url(${food})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", borderRadius:'16px', }}>
-        <Box sx={{textAlign:'center', color:'white', fontSize:'2rem', fontWeight:'500'}}><h6>Local Market of India</h6></Box>
+        <Typography Align='center' color='white' variant='h6' fontWeight='500'>Local Market of India</Typography>
         </Box>
         <Box sx={{display:'flex', width: '200px', height:'270px', justifyContent:'center', backgroundcolor:'Black', backgroundBlendMode:'Darken', background: 'rgba(0,0,0,0.5)', opacity:'0.8', alignItems:'center', backgroundImage:`url(${eco})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", borderRadius:'16px', }}>
-        <Box sx={{textAlign:'center', color:'white', fontSize:'2rem', fontWeight:'500'}}><h6>Eco Tourism in India</h6></Box>
+        <Typography Align='center' color='white' variant='h6' fontWeight='500'>Eco Tourism in India</Typography>
         </Box>
         <Box sx={{display:'flex', width: '200px', height:'270px', justifyContent:'center', backgroundcolor:'Black', backgroundBlendMode:'Darken', background: 'rgba(0,0,0,0.5)', opacity:'0.8', alignItems:'center', backgroundImage:`url(${Bc})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", borderRadius:'16px', }}>
-        <Box sx={{textAlign:'center', color:'white', fontSize:'2rem', fontWeight:'500'}}><h6>Food Blogs</h6></Box>
+        <Typography Align='center' color='white' variant='h6' fontWeight='500'>Food Blogs</Typography>
         </Box>
         </Box>
   )
--- Client/src/components/head/head.jsx
+++ Client/src/components/head/head.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import {Box} from "@mui/material";
+import {Typography, Box} from "@mui/material";
 import Bg from "../../images/Spiritual.jpg"
 
 const Head = () => {
   
   return (
         <Box sx={{display:'flex', width: '100%', height:'400px',justifyContent:'center', backgroundcolor:'Black',backgroundBlendMode:'Darken', background: 'rgba(0,0,0,0.5)' ,opacity:'0.8', alignItems:'center', backgroundImage:`url(${Bg})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover", backgroundAttachment: "fixed", borderRadius:'16px'}}>
-        
-        <Box sx={{align:'center', color:'white', fontWeight:500}}><h1>Discover the diversity and richness of India: A world within a country.</h1></Box>
+        <Box sx={{flexgrow:'1', width:{xs:'95%', sm:'80%', md:'75%', lg:'65%'}}}><Typography align='center' color='white'fontWeight='700' variant='h2'>Discover the diversity and richness of India: A world within a country.</Typography></Box>
         </Box>
   )
 }
